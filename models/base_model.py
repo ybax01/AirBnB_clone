7611670c0dedf01d823bdc7983edb73b9f73e4c6
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-11-17 19:54:27 +0100

    Air

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-BaseModel module defines the BaseModel class.
+This module defines the BaseModel class which serves as a base class
+for other models. It includes common attributes and methods.
 """
 
 import uuid
@@ -8,22 +9,25 @@ from datetime import datetime
 
 
 class BaseModel:
-    """
-    Defines the BaseModel class with common attributes and methods.
+    """Defines all common attributes/methods for other classes."""
 
-    Attributes:
-        id (str): Unique id assigned to each instance (UUID4).
-        created_at (datetime): Datetime when an instance is created.
-        updated_at (datetime): Datetime updated whenever the object changes.
-    """
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        Initializes a new instance of BaseModel.
+        Initializes a new BaseModel instance.
+        - If kwargs is provided and not empty, it creates attributes
+          from the key-value pairs in kwargs (except '__class__').
+        - Otherwise, it generates a new id and sets created_at and updated_at.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
@@ -34,21 +38,20 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the updated_at attribute to the current datetime.
+        Updates the public instance attribute `updated_at`
+        with the current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__.
-        Adds the __class__ key with the class name.
-        Converts created_at and updated_at to ISO 8601 format strings.
-
-        Returns:
-            dict: Dictionary representation of the instance.
+        Returns a dictionary representation of the instance.
+        - Includes all attributes in __dict__.
+        - Adds a `__class__` key with the class name.
+        - Converts created_at and updated_at to ISO format strings.
         """
-        dict_rep = self.__dict__.copy()
-        dict_rep['__class__'] = self.__class__.__name__
-        dict_rep['created_at'] = self.created_at.isoformat()
-        dict_rep['updated_at'] = self.updated_at.isoformat()
-        return dict_rep
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
